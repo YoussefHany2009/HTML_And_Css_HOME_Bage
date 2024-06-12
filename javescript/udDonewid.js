d7917fb1f21545ceaaa54c289f2011173d41020f
Youssef Hany <155045035+YoussefHany2009@users.noreply.github.com>
2024-06-12 16:10:19 -0700

    Add files via upload

---
+++ javescript/udDonewid.js
@@ -0,0 +1,20 @@
+//html Elemetns
+const UdDoneWid1 = document.getElementById("udDone-wid1");
+const UdDonemsg1 = document.getElementById("udDone-msg1");
+//Event Listener
+UdDoneWid1.addEventListener("mouseenter" , function(){
+    UdDonemsg1.style.display="block";
+})
+UdDoneWid1.addEventListener("mouseleave" , function(){
+    UdDonemsg1.style.display="none";
+})
+//html Elemetns
+const UdDoneWid2 = document.getElementById("udDone-wid2");
+const UdDonemsg2 = document.getElementById("udDone-msg2");
+//Event Listener
+UdDoneWid2.addEventListener("mouseenter" , function(){
+    UdDonemsg2.style.display="block";
+})
+UdDoneWid2.addEventListener("mouseleave" , function(){
+    UdDonemsg2.style.display="none";
+})
