5ab932b04019ac15a79e80cba273a8c074f9241e
Youssef Hany <155045035+YoussefHany2009@users.noreply.github.com>
2024-06-13 01:25:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 // HTML Elements
-const underworkwid = document.getElementById("Under-work");
-const msg = document.getElementById("msg");
-
-// Event Listener
-underworkwid.addEventListener("mouseenter", function(){
- msg.style.display = "block";
-});
-underworkwid.addEventListener("mouseleave", function(){
-    msg.style.display = "none";
-   });
 const donewid1 = document.getElementById("Done-wid1");
 const Donemsg1 = document.getElementById("Done-msg1");
+// Event Listener
 donewid1.addEventListener("mouseenter", function(){
     Donemsg1.style.display = "block";
    });
    donewid1.addEventListener("mouseleave", function(){
     Donemsg1.style.display = "none";
       });
+// HTML Elements
 const donewid2 = document.getElementById("Done-wid2");
 const Donemsg2 = document.getElementById("Done-msg2");
+// Event Listener
 donewid2.addEventListener("mouseenter", function(){
     Donemsg2.style.display = "block";
    });
    donewid2.addEventListener("mouseleave", function(){
     Donemsg2.style.display = "none";
       });
+// HTML Elements
+const donewid3 = document.getElementById("Done-wid3");
+const donemsg3 = document.getElementById("Done-msg3");
+//Event Listener
+donewid3.addEventListener("mouseenter" , function(){
+  donemsg3.style.display="block";
+})
+donewid3.addEventListener("mouseleave" , function(){
+  donemsg3.style.display="none";
+})
+// HTML Elements
+const donewid4 = document.getElementById("Done-wid4");
+const donemsg4 = document.getElementById("Done-msg4");
+//Event Listener
+donewid4.addEventListener("mouseenter" , function(){
+  donemsg4.style.display="block";
+})
+donewid4.addEventListener("mouseleave" , function(){
+  donemsg4.style.display="none";
+})
